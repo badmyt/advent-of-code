844855b478618cde3d03cb9e8a2657c50908d9e0
Dmytro <>
2022-12-20 18:55:12 +0100

    finish day 15

--- 2022/AdventOfCode/Days/Day15.cs
+++ 2022/AdventOfCode/Days/Day15.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCode.Days
 {
     public class Day15 : DayBase
     {
         private const string InputAlpha = "1234567890=,;-";
-        private readonly HashSet<(int, int)> _coveredPoints = new HashSet<(int, int)>();
-        private readonly HashSet<(int, int)> _coveredPointsMultipleTimes = new HashSet<(int, int)>();
-
-        private List<Sensor> _sensors = new List<Sensor>();
+        private HashSet<(int, int)> _beacons = new();
+        private List<Sensor> _sensors = new();
 
         public override void Run()
         {
@@ -26,7 +25,7 @@ namespace AdventOfCode.Days
                 .Select(x => new { Sensor = new Point(x[0],x[1]), Beacon = new Point(x[2], x[3])})
                 .ToList();
 
-            var beacons = pairs
+            _beacons = pairs
                 .Select(b => (b.Beacon.X, b.Beacon.Y))
                 .ToHashSet();
 
@@ -35,7 +34,7 @@ namespace AdventOfCode.Days
                 .ToList();
 
             var set = new HashSet<int>();
-            var controlY = 10;
+            var controlY = 20000;
 
             foreach (var sensor in _sensors)
             {
@@ -50,7 +49,7 @@ namespace AdventOfCode.Days
 
                 for (int x = startX; x <= endX; x++)
                 {
-                    if (!beacons.Contains((x, y)))
+                    if (!_beacons.Contains((x, y)))
                         set.Add(x);
                 }
             }
@@ -58,91 +57,42 @@ namespace AdventOfCode.Days
             Console.WriteLine($"There are {set.Count} positions that cannot contain beacon with y = {controlY}, elapsed = {sw.Elapsed}");
 
             Point beacon = null;
-
             foreach (var sensor in _sensors)
             {
-                if (sensor.Location.X == 8)
+                var y = sensor.Location.Y;
+                for (var x = sensor.Location.X - sensor.Radius - 1; x <= sensor.Location.X; x++)
                 {
-                    //debug
-                }
+                    if (!IsCoveredByAnySensor(x, y, sensor))
+                        beacon = new Point(x, y);
 
-                var startX = sensor.Location.X - sensor.Radius;
-                var endX = sensor.Location.X + sensor.Radius;
-                var leftX = startX - 1;
-                var rightX = endX + 1;
-
-                if (leftX == -6 || rightX == -6)
-                {
-                    //debug
-                }
-
-                beacon ??= CheckForBeacon(leftX, sensor.Location.Y, sensor);
-                beacon ??= CheckForBeacon(rightX, sensor.Location.Y, sensor);
-                if (beacon != null)
-                    break;
-
-                for (var x = startX; x <= endX; x++)
-                {
-                    if (x == -6)
-                    {
-                        //debug
-                    }
-
-                    var dx = Math.Abs(x - sensor.Location.X);
-                    var dy = Math.Abs(sensor.Radius - dx + 1);
-
-                    var yUpper = sensor.Location.Y - dy;
-                    var yLower = sensor.Location.Y + dy;
-                    var yZero = sensor.Location.Y;
-
-                    beacon ??= CheckForBeacon(x, yUpper, sensor);
-                    beacon ??= CheckForBeacon(x, yLower, sensor);
-                    if (beacon != null)
-                        break;
+                    y--;
                 }
             }
 
-            //var xor1 = _coveredPoints.Aggregate((0 ,0), (prev, cur) => { return (prev.Item1 ^ cur.Item1, prev.Item2 ^ cur.Item2); });
-            //var xor2 = _coveredPointsMultipleTimes.Aggregate(xor1, (prev, cur) => { return (prev.Item1 ^ cur.Item1, prev.Item2 ^ cur.Item2); });
-
-            //var tuningFrequency = xor2.Item1 * 4000000 + xor2.Item2;
-            var tuningFrequency = beacon == null ? 0 : beacon.X * 4_000_000 + beacon.Y;
+            var tuningFrequency = (BigInteger)beacon.X * 4_000_000 + beacon.Y;
 
             Console.WriteLine($"Beacon is at ({beacon?.X}, {beacon?.Y}), tuning frequency = {tuningFrequency}, elapsed = {sw.Elapsed}");
         }
 
-        private Point CheckForBeacon(int x, int y, Sensor caller)
+        private bool IsCoveredByAnySensor(int x, int y, Sensor caller)
         {
-            var maxValue = 20;
-            if (x > maxValue || y > maxValue || x < 0 || y < 0)
-                return null;
+            if (x < 0 || y < 0)
+                return true;
 
             if (_sensors.Except(new[] { caller }).Any(sensor => IsCoveredBySensor(new Point(x, y), sensor)))
-                return null;
+                return true;
 
-            return new Point(x,y);
+            return false;
         }
 
         private bool IsCoveredBySensor(Point point, Sensor sensor)
         {
             var dx = Math.Abs(point.X - sensor.Location.X);
             var dy = Math.Abs(point.Y - sensor.Location.Y);
-
-            if (dx > sensor.Radius || dy > sensor.Radius)
-                return false;
-
-            if (dx + dy <= sensor.Radius)
-                return true;
-
-            return false;
+            return dx + dy <= sensor.Radius;
         }
 
-        private int GetRadius(Point a, Point b)
-        {
-            var dx = Math.Abs(a.X - b.X);
-            var dy = Math.Abs(a.Y - b.Y);
-            return dx + dy;
-        }
+        private int GetRadius(Point a, Point b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
 
         private class Sensor {
             public Point Location;
