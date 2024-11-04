ae28e70549c284d4c4da31cc295bfde4cc2fca57
badmyt <>
2024-11-04 18:39:06 +0100

    add dynamic day runner

---
+++ AdventOfCode/2023/Days/Day10.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static AdventOfCode.Extensions;
+
+namespace AdventOfCode.Days
+{
+    public class Day10 : DayBase
+    {
+        public Day10(int year) : base(year) { }
+
+        public override void Run()
+        {
+            var lines = File.ReadAllLines(InputPath);
+
+            
+            
+            Console.WriteLine($"");
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using AdventOfCode.Days;
 
 namespace AdventOfCode
 {
     class Program
     {
+        private const int Year = 2023;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of code console!");
             var sw = new Stopwatch();
             sw.Start();
 
-            var day = new Day9(2023);
-            day.Run();
+            // Get all types in the current assembly that match the pattern DayX and inherit from DayBase
+            var dayType = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsClass && t.Name.StartsWith("Day") && t.BaseType?.Name == nameof(DayBase))
+                .OrderByDescending(t => int.Parse(t.Name.Substring(3)))  // Extract number and sort descending
+                .FirstOrDefault();  // Get the class with the highest number
+
+            if (dayType != null)
+            {
+                // Create an instance of the highest DayX class and call Run
+                var dayInstance = (DayBase)Activator.CreateInstance(dayType, Year); // Assuming constructor takes year
+
+                var dayClassName = dayInstance.GetType().Name;
+                var dayNumber = dayClassName.Substring(3);
+                Console.WriteLine($"Running day {dayNumber}...");
+                dayInstance.Run();
+            }
+            else
+            {
+                Console.WriteLine("No DayX classes found.");
+            }
 
             Console.WriteLine("Execution time - " + sw.Elapsed);
             Console.WriteLine("Press enter to exit..");
